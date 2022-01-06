76491f8f53421d64e5535e2d3d31e5ce12b2650e
Abhinav Gupta <abhinavg1211@gmail.com>
2022-01-07 01:33:05 +0530

    added styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './images/memories.png';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import srcStyles from './styles'
+
 
 const App = () => {
 
+    const styleClasses = srcStyles();
+
     return (
-        <div>
-            <h1> App </h1>
-        </div>
+        <Container maxWidth = "lg">
+
+            <AppBar className = {styleClasses.appBar} position='static' color='inherit'>
+
+                <Typography className = {styleClasses.heading} variant='h2' align='center'> Reminiscence </Typography> 
+
+                <img className={styleClasses.image} src={memories} alt='memories' height='60' />
+
+            </AppBar>
+
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+
+                        <Grid item xs = {12} sm = {7}>
+                            <Posts/>
+                        </Grid>
+
+                        <Grid item xs = {12} sm = {4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+
+        </Container>
     )
 }
 
