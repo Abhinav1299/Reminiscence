7548f3033e6d3b5d9f776f3a7c093096dbe28c46
Abhinav1299 <abhinavg1211@gmail.com>
2022-05-03 17:31:04 +0530

    backend URL update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const url = 'https://reminiscence-1.herokuapp.com/posts';
-const API = axios.create({ baseURL: "http://localhost:5000" });
+// const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: process.env.REACT_APP_BACKEND_URL });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
